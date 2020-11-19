16cf1b0b6e84c0209c463074df08d32fdd9430a4
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-11-20 00:33:14 +0100

    resync

--- pipething.js
+++ pipething.js
@@ -4,22 +4,21 @@ var minimist = require('minimist')
 var dedent = require('dedent')
 
 var help = dedent`
-    Usage: pipething <topic>
+    Usage: pipething <topic> <optional server>
     Examples:
       # TODO
       echo hello | pipething test123
 `
 var args = minimist(process.argv.slice(2))
-var topic = args._[0];
+var topic = args._[0] || 'pipething';
+var url = args._[1] || 'wss://de.meething.space:443/'+topic;
 
 var config = {
-  url: 'wss://de.meething.space:443/'+topic,
+  url: url,
   key: topic
 }
 var mesh = Mesh(config);
 
-mesh.pipe( (ev) => {
-  process.stdout.write(Buffer.from(ev.data));
-  });
+mesh.pipe( (ev) => { process.stdout.write(Buffer.from(ev.data)) });
 process.stdin.on('data', data => { mesh.sendToAll(data) });
 
